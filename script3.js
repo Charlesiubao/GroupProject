b4c6d9232ec60c6f0e282cb395c09730ae675592
Natalie <theliteshado@icloud.com>
2021-03-25 11:52:15 -0400

    extended script

--- script3.js
+++ script3.js
@@ -2,21 +2,58 @@
 // books of data
 
 let booksID = null
-(list all, add new)
 
-const bookIndex = async ()
- => {
+const bookIndex = async () => {
 
-document.querySelector(#list-all).classList.remove(‘hidden’)
-document.querySelector(#add-new).classList.remove(‘hidden’)
-document.querySelector(‘#new’).classList.add(‘hidden’)
-document.querySelector(‘#edit’).classList.add(‘hidden’)
+// document.querySelector('.allBooks').classList.remove(‘hidden’)
+// document.querySelector('.edits').classList.remove(‘hidden’)
+// document.querySelector(‘#new’).classList.add(‘hidden’)
+// document.querySelector(‘#edit’).classList.add(‘hidden’)
 
-const res = await fetch(‘http://myapi-profstream.herokuapp.com/api/6e5dd1/books')
+const res = await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books`)
 const data = await res.json()
 
-document.querySelector(‘#list-all’).textContent = ‘’
+document.querySelector(`#listall`).textContent = ‘’
 
-for (books of data) {
+for (book of data) {
 
-	const
+	const bookDiv = document.createElement(`div`)
+    bookDiv.classList.add(`single-book`)
+    bookDiv.innerText = `${book.title}, from ${book.author}`
+    bookDiv.id = 'book-` + book.id
+    document.querySelector('#listall').append(bookDiv)
+
+}
+}
+
+
+const goToShow = async (id) => {
+    const res = await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books/${id}`)
+    const data = await res.json()
+    document.createElement(`div`)
+    document.classList.add(`show-name`)
+    document.classList.add(`show-book-author`)
+    document.classList.add(`show-release`)
+    document.classList.add(`show-cover`)
+    document.querySelector(`.show-name`).innerText = data.name
+    document.querySelector(`.show-book-author`).innerText = `${data.author}`
+    document.querySelector(`.show-title`).innerText = data.title
+    document.querySelector(`.show-release`).innerText = data.release_year
+    document.querySelector(`.show-cover`).src = data.image
+
+    document.querySelector('#new').classList.add('hidden')
+    document.querySelector('#edit').classList.add('hidden')
+    document.querySelector('#listall').classList.remove('hidden')
+    document.querySelector('#addnew').classList.add('hidden')
+
+
+    document.querySelector('#listall').addEventListener('click', goToShow)
+
+    document.querySelector('#new-book').addEventListener('click', () => {
+    document.querySelector('#new').classList.remove('hidden')
+    // document.querySelector('#edit').classList.add('hidden')
+    // document.querySelector('#show').classList.add('hidden')
+    // document.querySelector('#listall').classList.add('hidden')
+})
+
+}
