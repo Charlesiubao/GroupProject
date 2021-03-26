7a696b2bcf6e1988dcff09b7131827978e3c258e
Charlesiubao <kristoffer.charles.reyes@gmail.com>
2021-03-26 10:45:28 -0700

    making it work

--- script.js
+++ script.js
@@ -26,15 +26,101 @@ const showAllBooks = async () => {
         for (book of data) {
             const bookDiv = document.createElement('div')
             bookDiv.classList.add('single-book')
-            bookDiv.innerText = `${book.name}, from ${book.author}`
+            bookDiv.innerText = `${book.title}, from ${book.author}`
+            bookDiv.src = `${book.image}`
             bookDiv.id = 'book-' + book.id
             document.querySelector('#allBooks').append(bookDiv)
     }
 }
 
-
+document.querySelector('#listAll').addEventListener('click', () => {
+    allBooks.classList.add('inactive')
+    edits.classList.add('inactive')
+    newAddition.classList.remove('inactive')
+    frontPage.classList.add('inactive')
+    view.classList.add('inactive')
+})
 
 //addBooks 
+
+const addBooks = () => {
+    allBooks.classList.add('inactive')
+    edits.classList.add('inactive')
+    newAddition.classList.remove('inactive')
+    frontPage.classList.add('inactive')
+    view.classList.add('inactive')
+    return
+}
+
+const toggleView = async (id) => {
+    const res = await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books/${id}`)
+    const data = await res.json()
+
+    document.querySelector('#viewTitle').innerText = data.title
+    document.querySelector('#viewAuthor').innerText = data.author
+    document.querySelector('#viewReleaseDate').innerText = data.release_date
+    document.querySelector('#viewImage').src = data.image
+
+    allBooks.classList.add('inactive')
+    edits.classList.add('inactive')
+    newAddition.classList.add('inactive')
+    frontPage.classList.add('inactive')
+    view.classList.remove('inactive')
+
+}
+
+
+
+
+//Actions
+const toggleFrontPage = () => {
+    frontPage.classList.remove('inactive')
+    allBooks.classList.add('inactive')
+    edits.classList.add('inactive')
+    newAddition.classList.add('inactive')
+    view.classList.add('inactive')
+    return
+}
+
+//delete
+document.querySelector('#deleteBook').addEventListener('click', async () => {
+    await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books/${bookId}`, {
+        method: 'DELETE'
+    })
+    showAllBooks()
+})
+
+//Edit
+document.querySelector('#editBook').addEventListener('click', (event) => {
+    edit(bookId)
+})
+
+const edit = async (id) => {
+
+    const res = await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books/${id}`)
+    const book = await res.json()
+
+    document.querySelector('#editBook').value = book.title
+    document.querySelector('#editAuthor').value = book.author
+    document.querySelector('#editReleaseDate').value = book.release_date
+
+    allBooks.classList.add('inactive')
+    edits.classList.remove('inactive')
+    newAddition.classList.add('inactive')
+    frontPage.classList.add('inactive')
+    view.classList.add('inactive')
+    return
+}
+
+document.addEventListener('click', async (event) => {
+    if (event.target.matches('.single-book')) {
+        bookId = event.target.id.replace('book-', '')
+        toggleView(bookId)
+    }
+})
+
+
+
 
 document.querySelector("#newBookForm").addEventListener('submit', async (event) => {
     event.preventDefault()
@@ -61,66 +147,7 @@ document.querySelector("#newBookForm").addEventListener('submit', async (event) 
     toggleView(data.id)
 })
 
-
-
-
-
-//Actions
-const toggleFrontPage = () => {
-    frontPage.classList.remove('inactive')
-    allBooks.classList.add('inactive')
-    edits.classList.add('inactive')
-    newAddition.classList.add('inactive')
-    view.classList.add('inactive')
-    return
-}
-
-//Edit
-const edit = async (id) => {
-
-    const res = await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books/${id}`)
-    const book = await res.json()
-
-    document.querySelector('#editBook').value = book.title
-    document.querySelector('#editAuthor').value = book.author
-    document.querySelector('#editReleaseDate').value = book.release_date
-
-    allBooks.classList.add('inactive')
-    edits.classList.remove('inactive')
-    newAddition.classList.add('inactive')
-    frontPage.classList.add('inactive')
-    view.classList.add('inactive')
-    return
-}
-
-const addBooks = () => {
-    allBooks.classList.add('inactive')
-    edits.classList.add('inactive')
-    newAddition.classList.remove('inactive')
-    frontPage.classList.add('inactive')
-    view.classList.add('inactive')
-    return
-}
-
-
-
-const toggleView = async (id) => {
-    const res = await fetch(`http://myapi-profstream.herokuapp.com/api/6e5dd1/books/${id}`)
-    const data = await res.json()
-
-    document.querySelector('#viewTitle').innerText = data.title
-    document.querySelector('#viewAuthor').innerText = data.author
-    document.querySelector('#viewReleaseDate').innerText = data.release_date
-    document.querySelector('#viewImage').src = data.image
-
-    allBooks.classList.add('inactive')
-    edits.classList.add('inactive')
-    newAddition.classList.add('inactive')
-    frontPage.classList.add('inactive')
-    view.classList.remove('inactive')
-
-}
-
+// const res = await
 
 
 
@@ -128,6 +155,7 @@ const toggleView = async (id) => {
 const listAll = document.querySelector('#listAll')
 listAll.addEventListener('click', () => {
     showAllBooks()
+    
     pageTurn.play()
     // console.log('listAll')
 })
